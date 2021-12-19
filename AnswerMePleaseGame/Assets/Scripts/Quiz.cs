1784afb01ddadb0a087053d18bb9bcd2998b4bef
Miles Debinski <miles.debinski@gmail.com>
2021-12-19 21:29:36 +0000

    setup game manager

--- AnswerMePleaseGame/Assets/Scripts/Quiz.cs
+++ AnswerMePleaseGame/Assets/Scripts/Quiz.cs
@@ -14,7 +14,7 @@ public class Quiz : MonoBehaviour
   [Header("Answers")]
   [SerializeField] GameObject[] answerButtons;
   int correctAnswerIndex;
-  bool hasAnsweredEarly;
+  bool hasAnsweredEarly = true;
 
   [Header("Button Colors")]
   [SerializeField] Sprite defaultAnswerSprite;
@@ -33,7 +33,7 @@ public class Quiz : MonoBehaviour
 
   public bool isComplete;
 
-  void Start()
+  void Awake()
   {
     timer = FindObjectOfType<Timer>();
     scoreKeeper = FindObjectOfType<ScoreKeeper>();
@@ -44,10 +44,14 @@ public class Quiz : MonoBehaviour
 
   void Update()
   {
-    // timerImage.GetComponent<Image>().fillAmount = timer.fillFraction;
     timerImage.fillAmount = timer.fillFraction;
     if (timer.loadNextQuestion)
     {
+      if (progressBar.value == progressBar.maxValue)
+      {
+        isComplete = true;
+        return;
+      }
       hasAnsweredEarly = false;
       GetNextQuestion();
       timer.loadNextQuestion = false;
@@ -66,11 +70,6 @@ public class Quiz : MonoBehaviour
     SetButtonState(false);
     timer.CancelTimer();
     scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
-
-    if (progressBar.value == progressBar.maxValue)
-    {
-      isComplete = true;
-    }
   }
 
   void DisplayAnswer(int index)
